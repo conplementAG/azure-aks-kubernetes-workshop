30921688acd1a79187325c3ca2f8dca25a07966d
Peter Ittner <peter.ittner.cp@hotmail.com>
2019-07-04 08:57:57 +0200

    feat: updates description for swagger

--- example-application/Backend/Startup.cs
+++ example-application/Backend/Startup.cs
@@ -58,7 +58,7 @@ namespace Backend
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo App API");
             });
 
             
