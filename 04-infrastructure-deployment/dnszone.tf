eaf3cc9b41b071f8fec1266044489d51b9600521
Tamas <nemeth.t@gmail.com>
2019-11-28 15:30:09 +0100

    Revert "DONOTMERGE: temporary fix to work without AAD"
    This reverts commit fc8b30d0d2f56d368216512bef2c3cb2401329c3.

--- 04-infrastructure-deployment/dnszone.tf
+++ 04-infrastructure-deployment/dnszone.tf
@@ -1,76 +1,76 @@
-# resource "random_uuid" "external-dns-sp-password" {
-# }
+resource "random_uuid" "external-dns-sp-password" {
+}
 
-# locals {
-#   external_dns_sp_name = "aksws-${var.short_region}-${var.env_tag}-external-dns-service-principal"
-# }
+locals {
+  external_dns_sp_name = "aksws-${var.short_region}-${var.env_tag}-external-dns-service-principal"
+}
 
-# # DNS Zone
+# DNS Zone
 
-# resource "azurerm_dns_zone" "dnszone" {
-#   name                = "aksws.conplement.cloud" # change aksws to your env-tag! TODO: replace with variable
-#   resource_group_name = local.resource_group_name
-#   zone_type           = "Public"
-# }
+resource "azurerm_dns_zone" "dnszone" {
+  name                = "aksws.conplement.cloud" # change aksws to your env-tag! TODO: replace with variable
+  resource_group_name = local.resource_group_name
+  zone_type           = "Public"
+}
 
-# output "dns_servers" {
-#   value = azurerm_dns_zone.dnszone.name_servers
-# }
+output "dns_servers" {
+  value = azurerm_dns_zone.dnszone.name_servers
+}
 
-# # Service Principal for Managing the DNS Zone
-# # https://github.com/kubernetes-incubator/external-dns/blob/master/docs/tutorials/azure.md
+# Service Principal for Managing the DNS Zone
+# https://github.com/kubernetes-incubator/external-dns/blob/master/docs/tutorials/azure.md
 
-# data "azurerm_resource_group" "resource-group" {
-#   name = local.resource_group_name
-# }
+data "azurerm_resource_group" "resource-group" {
+  name = local.resource_group_name
+}
 
-# resource "azuread_application" "external-dns-app" {
-#   name                       = local.external_dns_sp_name
-#   homepage                   = "https://${local.external_dns_sp_name}"
-#   identifier_uris            = ["https://${local.external_dns_sp_name}"]
-#   reply_urls                 = ["https://${local.external_dns_sp_name}"]
-#   available_to_other_tenants = false
-# }
+resource "azuread_application" "external-dns-app" {
+  name                       = local.external_dns_sp_name
+  homepage                   = "https://${local.external_dns_sp_name}"
+  identifier_uris            = ["https://${local.external_dns_sp_name}"]
+  reply_urls                 = ["https://${local.external_dns_sp_name}"]
+  available_to_other_tenants = false
+}
 
-# resource "azuread_service_principal" "external-dns-sp" {
-#   application_id = azuread_application.external-dns-app.application_id
-# }
+resource "azuread_service_principal" "external-dns-sp" {
+  application_id = azuread_application.external-dns-app.application_id
+}
 
-# resource "azuread_service_principal_password" "external-dns-sp-password" {
-#   service_principal_id = azuread_service_principal.external-dns-sp.id
-#   value                = random_uuid.external-dns-sp-password.result
-#   end_date             = timeadd(timestamp(), "8760h") # 1 year = 365 * 24 hours
-# }
+resource "azuread_service_principal_password" "external-dns-sp-password" {
+  service_principal_id = azuread_service_principal.external-dns-sp.id
+  value                = random_uuid.external-dns-sp-password.result
+  end_date             = timeadd(timestamp(), "8760h") # 1 year = 365 * 24 hours
+}
 
-# resource "azurerm_role_assignment" "external-dns-sp-rg-reader" {
-#   scope                = data.azurerm_resource_group.resource-group.id
-#   role_definition_name = "Reader"
-#   principal_id         = azuread_service_principal.external-dns-sp.id
-# }
+resource "azurerm_role_assignment" "external-dns-sp-rg-reader" {
+  scope                = data.azurerm_resource_group.resource-group.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.external-dns-sp.id
+}
 
-# resource "azurerm_role_assignment" "external-dns-sp-dnszone-contributor" {
-#   scope                = azurerm_dns_zone.dnszone.id
-#   role_definition_name = "Contributor"
-#   principal_id         = azuread_service_principal.external-dns-sp.id
-# }
+resource "azurerm_role_assignment" "external-dns-sp-dnszone-contributor" {
+  scope                = azurerm_dns_zone.dnszone.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.external-dns-sp.id
+}
 
-# resource "kubernetes_secret" "azure-config-file" {
-#   metadata {
-#     name = "azure-config-file"
-#   }
+resource "kubernetes_secret" "azure-config-file" {
+  metadata {
+    name = "azure-config-file"
+  }
 
-#   data = {
-#     "azure.json" = <<EOF
-#     {
-#       "tenantId": "${var.tenant_id}",
-#       "subscriptionId": "${var.subscription_id}",
-#       "resourceGroup": "${data.azurerm_resource_group.resource-group.name}",
-#       "aadClientId":"${azuread_application.external-dns-app.application_id}",
-#       "aadClientSecret": "${azuread_service_principal_password.external-dns-sp-password.value}"
-#     }
+  data = {
+    "azure.json" = <<EOF
+    {
+      "tenantId": "${var.tenant_id}",
+      "subscriptionId": "${var.subscription_id}",
+      "resourceGroup": "${data.azurerm_resource_group.resource-group.name}",
+      "aadClientId":"${azuread_application.external-dns-app.application_id}",
+      "aadClientSecret": "${azuread_service_principal_password.external-dns-sp-password.value}"
+    }
     
-# EOF
+EOF
 
-#   }
-# }
+  }
+}
 
--- 04-infrastructure-deployment/keyvault.tf
+++ 04-infrastructure-deployment/keyvault.tf
@@ -1,41 +1,41 @@
-# locals {
-#   keyvault_name = "aksws-${var.short_region}-${var.env_tag}-kv"
-# }
+locals {
+  keyvault_name = "aksws-${var.short_region}-${var.env_tag}-kv"
+}
 
-# data "azurerm_client_config" "client" {
-# }
+data "azurerm_client_config" "client" {
+}
 
-# resource "azurerm_key_vault" "keyvault" {
-#   name                        = local.keyvault_name
-#   location                    = var.long_region
-#   resource_group_name         = local.resource_group_name
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = var.tenant_id
-#   sku_name                    = "standard"
+resource "azurerm_key_vault" "keyvault" {
+  name                        = local.keyvault_name
+  location                    = var.long_region
+  resource_group_name         = local.resource_group_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
 
-#   access_policy {
-#     tenant_id = var.tenant_id
-#     object_id = data.azurerm_client_config.client.service_principal_object_id
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = data.azurerm_client_config.client.service_principal_object_id
 
-#     key_permissions     = ["get", "list", "update", "create", "import", "delete", "recover", "backup", "restore"]
-#     secret_permissions  = ["get", "list", "set", "delete", "recover", "backup", "restore"]
-#     storage_permissions = ["get", "list", "update", "delete", "recover", "backup", "restore"]
-#   }
+    key_permissions     = ["get", "list", "update", "create", "import", "delete", "recover", "backup", "restore"]
+    secret_permissions  = ["get", "list", "set", "delete", "recover", "backup", "restore"]
+    storage_permissions = ["get", "list", "update", "delete", "recover", "backup", "restore"]
+  }
 
-#   tags = {
-#     environment = "Production"
-#   }
-# }
+  tags = {
+    environment = "Production"
+  }
+}
 
-# resource "azurerm_role_assignment" "keyvault_role_assignment" {
-#   scope                = azurerm_key_vault.keyvault.id
-#   role_definition_name = "Reader"
-#   principal_id         = azuread_service_principal.cluster_service_principal.id
-# }
+resource "azurerm_role_assignment" "keyvault_role_assignment" {
+  scope                = azurerm_key_vault.keyvault.id
+  role_definition_name = "Reader"
+  principal_id         = azuread_service_principal.cluster_service_principal.id
+}
 
-# resource "azurerm_key_vault_secret" "test_secret" {
-#   name         = "test-secret"
-#   value        = "secret-secret"
-#   key_vault_id = azurerm_key_vault.keyvault.id
-# }
+resource "azurerm_key_vault_secret" "test_secret" {
+  name         = "test-secret"
+  value        = "secret-secret"
+  key_vault_id = azurerm_key_vault.keyvault.id
+}
 
